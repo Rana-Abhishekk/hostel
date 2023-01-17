feb3caddc165355df6b782d448d119f9b239077f
Abhishek <abhishek17082001@gmail.com>
2023-01-18 01:38:39 +0530

    first

---
+++ check_availability.php
@@ -0,0 +1,96 @@
+<?php
+require_once("includes/config.php");
+
+//For Email
+if(!empty($_POST["emailid"])) {
+	$email= $_POST["emailid"];
+	if (filter_var($email, FILTER_VALIDATE_EMAIL)===false) {
+
+		echo "error : You did not enter a valid email.";
+	}
+	else {
+		$result ="SELECT count(*) FROM userRegistration WHERE email=?";
+		$stmt = $mysqli->prepare($result);
+		$stmt->bind_param('s',$email);
+		$stmt->execute();
+$stmt->bind_result($count);
+$stmt->fetch();
+$stmt->close();
+if($count>0)
+{
+echo "<span style='color:red'> Email already exist. Please try again.</span>";
+}
+}
+}
+// For Registration Number
+if(!empty($_POST["regno"])) {
+	$regno= $_POST["regno"];
+
+		$result ="SELECT count(*) FROM userRegistration WHERE regNo=?";
+		$stmt = $mysqli->prepare($result);
+		$stmt->bind_param('s',$regno);
+		$stmt->execute();
+$stmt->bind_result($count);
+$stmt->fetch();
+$stmt->close();
+if($count>0)
+{
+echo "<span style='color:red'> Registration number already exist. Please try again .</span>";
+}
+
+}
+
+
+// For old Password
+if(!empty($_POST["oldpassword"])) 
+{
+$pass=$_POST["oldpassword"];
+$result ="SELECT password FROM userregistration WHERE password=?";
+$stmt = $mysqli->prepare($result);
+$stmt->bind_param('s',$pass);
+$stmt->execute();
+$stmt -> bind_result($result);
+$stmt -> fetch();
+$opass=$result;
+if($opass==$pass) 
+echo "<span style='color:green'> Password  matched .</span>";
+else echo "<span style='color:red'> Password Not matched</span>";
+}
+
+// For room availbilty
+if(!empty($_POST["roomno"])) 
+{
+$roomno=$_POST["roomno"];
+$result ="SELECT count(*) FROM registration WHERE roomno=?";
+$stmt = $mysqli->prepare($result);
+$stmt->bind_param('i',$roomno);
+$stmt->execute();
+$stmt->bind_result($count);
+$stmt->fetch();
+$stmt->close();
+
+
+$resultSeater ="SELECT seater FROM rooms WHERE room_no=?";
+$stmtS = $mysqli->prepare($resultSeater);
+$stmtS->bind_param('i',$roomno);
+$stmtS->execute();
+$stmtS->bind_result($countS);
+$stmtS->fetch();
+$stmtS->close();
+
+
+if($count>0){
+	if($count>$countS){
+		echo "<span style='color:red'> Seats already full Room can't be allocated.</span>";
+		echo '
+		<script>
+		   document.getElementById("register").disabled = true;
+		</script> '  ;
+	}else{
+echo "<span style='color:red'>$count Seats already full.</span>";
+	}
+}
+else
+	echo "<span style='color:red'>All Seats are Available</span>";
+}
+?>
